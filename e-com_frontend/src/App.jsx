fceb393002a4f065ccd86bb88ce8ba9467d803ba
Chhavi Jhari <Chhavi.Jhari@Coforge.com>
2025-06-24 12:57:39 +0530

    added basic navbar searchbar footer components

--- e-com_frontend/src/App.jsx
+++ e-com_frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Navbar } from './components/Navbar';
+import { NavBar } from './components/Navbar';
 import { Footer } from './components/Footer';
+import { SearchBar } from './components/Searchbar'
 import { Home } from './pages/Home';
 import { Collection } from './pages/Collection';
 import { Cart } from './pages/Cart';
@@ -10,11 +11,15 @@ import { Contact } from './pages/Contact';
 import { Privacy } from './pages/Privacy';
 import { Terms } from './pages/Terms';
 import { DataProvider } from './utils/DataContext';
-export function App() {
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+export default function App() {
   return <BrowserRouter>
       <DataProvider>
         <div className="flex flex-col min-h-screen bg-gray-50">
-          <Navbar />
+          <NavBar />
+          <SearchBar />
           <main className="flex-grow">
             <Routes>
               <Route path="/" element={<Home />} />
